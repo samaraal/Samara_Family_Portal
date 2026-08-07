749df2775d6ffa1bf5c298a69a9b3ba81caa8189
samaraal <samarahealthcare2026@gmail.com>
2026-08-07 20:40:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.SAMARA_FAMILY_CONFIG = Object.freeze({
+  supabaseUrl: 'https://askalabwtlrnoodinayq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_MPf0spA1IsJWWR5-ltVAyA_Z_gctBr-',
+  version: '1.0.1'
+});
